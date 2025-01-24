e8d7a396931e9ab8007f99abaa57a00f40148e59
ritik23saini <ritik23saini@gmail.com>
2025-01-24 23:27:07 +0530

    db.js

--- backend/src/utils/db.js
+++ backend/src/utils/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect(process.env.Mongodb_Url, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
