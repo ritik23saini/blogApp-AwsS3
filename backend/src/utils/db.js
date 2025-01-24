e9eda2f07cc1cb240b69b967b936052c793d1ddc
ritik23saini <ritik23saini@gmail.com>
2025-01-25 01:17:43 +0530

    index.js

--- backend/src/utils/db.js
+++ backend/src/utils/db.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try {
+        console.log('MongoDB URI:', process.env.Mongodb_Url);
         await mongoose.connect(process.env.Mongodb_Url).then(() => {
             console.log("Database connected");
             return;
