987ac50f5683ba9b6d42e2b2095c614776264aad
ritik23saini <ritik23saini@gmail.com>
2025-01-24 21:23:36 +0530

    axios

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
  const axiosInstance = axios.create({
-    baseURL: "http://localhost:5002/", 
+    baseURL: "http://localhost:8000/", //import.meta.env.BASE_URL,
     withCredentials: true,
   });
   export default axiosInstance;
